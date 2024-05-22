4fe4b1fd499d5967884bf36d746c20415df1deb6
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2024-05-22 10:22:04 +0300

    handel get all brache

--- restaurant-chain/back end/controllers/branch.js
+++ restaurant-chain/back end/controllers/branch.js
@@ -1,36 +1,56 @@
 const Branch = require("../models/branch");
 
-
-
 exports.addBranch = async (req, res, next) => {
-    const { name, phone, street_name} = req.body;
-  
-    try {
-      const result = await Branch.findOrCreate({
-        where: { name },
-        defaults: {
-          name,
-          phone,
-          street_name,
-        
-        },
+  const { name, phone, street_name } = req.body;
+
+  try {
+    const result = await Branch.findOrCreate({
+      where: { name },
+      defaults: {
+        name,
+        phone,
+        street_name,
+      },
+    });
+
+    if (!result[0]._options.isNewRecord) {
+      return res.status(401).json({
+        error: true,
+        message: "Branch Already Exist",
       });
-  
-      if (!result[0]._options.isNewRecord) {
-        return res.status(401).json({
-          error: true,
-          message: "Branch Already Exist",
-        });
-      }
-  
+    }
+
+    return res.status(200).json({
+      error: false,
+      message: "Branch Created Successfully",
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.getAllBranches = async (req, res, next) => {
+  try {
+    const result = await Branch.findAll();
+    console.log("====================================", result.length);
+    if (result[0]?._options.raw) {
       return res.status(200).json({
         error: false,
-        message: "Branch Created Successfully",
+        all_Branches: result,
       });
-    } catch (err) {
-      if (!err.statusCode) {
-        err.statusCode = 500;
+    }else if (result.length === 0) {
+        return res.status(201).json({
+          error: false,
+          message: "no branch added yet",
+        });
       }
-      next(err);
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
     }
-  };
+    next(err);
+  }
+};
--- restaurant-chain/back end/routes/branch.js
+++ restaurant-chain/back end/routes/branch.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const BranchRouter = express.Router();
-const { addBranch } = require("../controllers/branch");
+const { addBranch,getAllBranches } = require("../controllers/branch");
 // const { authentication } = require("../middlewares/authintication");
 // const { authorization } = require("../middlewares/authurization");
 
 
 BranchRouter.post("/", addBranch);
+BranchRouter.get("/", getAllBranches);
 
 module.exports = BranchRouter;
